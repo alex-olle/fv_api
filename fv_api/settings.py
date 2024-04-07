9cb2803d9bdce6030a11cdd16b1b0d69df9aeef4
Àlex Ollé <alex.olle.93@gmail.com>
2024-04-07 16:49:32 +0200

    Migrate DB to SUPABASE

--- fv_api/settings.py
+++ fv_api/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,8 +91,12 @@ WSGI_APPLICATION = "fv_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": "aws-0-eu-central-1.pooler.supabase.com",
+        "NAME": "postgres",
+        "USER": os.getenv("SUPABASE_USER"),
+        "PORT": "5432",
+        "PASSWORD": os.getenv("SUPABASE_PASSWORD"),
     }
 }
 
